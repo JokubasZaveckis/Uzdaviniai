02de05443194e61df083303f917c8bf09967569f
Jokubas123456 <jokubas.zaveckis@gmail.com>
2022-01-15 22:47:49 +0200

    Egzamino_uzbaigimas

--- 2021/Pagrindine/Apklausa/main.cpp
+++ 2021/Pagrindine/Apklausa/main.cpp
@@ -19,12 +19,14 @@ struct Pamoka
     string pavadinimas;
     int kiekisMokiniu=0;
     string mokiniai[50];
-    int vidurkiai[50];
+    float vidurkiai[50];
+    int kiekVirs9=0;
 };
 
 void Nuskaitymas(int&m, Mokinys*& mokiniai);
 void VidurkioSkaiciavimas(int m, Mokinys*& mokiniai);
 void PamokuAtrinkimas(int m, Mokinys*& mokiniai, Pamoka*& pamokos, int& kiekPamoku);
+void VidurkioPatikrinimas(int kiekPamoku, Pamoka*& pamokos);
 void Rikiavimas(int kiekPamoku, Pamoka*& pamokos);
 void Isvedimas(int kiekPamoku, Pamoka*& pamokos);
 
@@ -37,6 +39,7 @@ int main()
     Nuskaitymas(m, mokiniai);
     VidurkioSkaiciavimas(m, mokiniai);
     PamokuAtrinkimas(m, mokiniai, pamokos, kiekPamoku);
+    VidurkioPatikrinimas(kiekPamoku, pamokos);
     Rikiavimas(kiekPamoku, pamokos);
     Isvedimas(kiekPamoku, pamokos);
 
@@ -83,24 +86,40 @@ void PamokuAtrinkimas(int m, Mokinys*& mokiniai, Pamoka*& pamokos, int& kiekPamo
     pamokos[kiekPamoku].mokiniai[pamokos[kiekPamoku].kiekisMokiniu] = mokiniai[0].vardas;
     pamokos[kiekPamoku].vidurkiai[pamokos[kiekPamoku].kiekisMokiniu++] = mokiniai[0].vidurkis;
     kiekPamoku++;
-    for(int i=0; i<m; i++)
+    for(int i=1; i<m; i++)
     {
         bool rasta=false;
         for(int j=0; j<kiekPamoku; j++)
         {
-            if(mokiniai[i].megstamiausiaPamoka==mokiniai[j].megstamiausiaPamoka)
+            if(mokiniai[i].megstamiausiaPamoka==pamokos[j].pavadinimas)
             {
                 rasta=true;
                 pamokos[j].mokiniai[pamokos[j].kiekisMokiniu] = mokiniai[i].vardas;
-                pamokos[j].vidurkiai[pamokos[j].kiekisMokiniu++] = mokiniai[i].vidurkis;
+                pamokos[j].vidurkiai[pamokos[j].kiekisMokiniu] = mokiniai[i].vidurkis;
+                pamokos[j].kiekisMokiniu++;
             }
         }
         if(rasta==false)
         {
-            pamokos[kiekPamoku].pavadinimas = pamokos[i].pavadinimas;
+            pamokos[kiekPamoku].pavadinimas = mokiniai[i].megstamiausiaPamoka;
             pamokos[kiekPamoku].mokiniai[pamokos[kiekPamoku].kiekisMokiniu] = mokiniai[i].vardas;
-            pamokos[kiekPamoku].vidurkiai[pamokos[kiekPamoku].kiekisMokiniu++] = mokiniai[i].vidurkis;
+            pamokos[kiekPamoku].vidurkiai[pamokos[kiekPamoku].kiekisMokiniu] = mokiniai[i].vidurkis;
+            pamokos[kiekPamoku].kiekisMokiniu++;
             kiekPamoku++;
+        }
+    }
+}
+
+void VidurkioPatikrinimas(int kiekPamoku, Pamoka*& pamokos)
+{
+    for(int i=0; i<kiekPamoku; i++)
+    {
+        for(int j=0; j<pamokos[i].kiekisMokiniu; j++)
+        {
+            if(pamokos[i].vidurkiai[j]>=9)
+            {
+                pamokos[i].kiekVirs9++;
+            }
         }
     }
 }
@@ -112,7 +131,7 @@ void Rikiavimas(int kiekPamoku, Pamoka*& pamokos)
         int max=i;
         for(int j=i+1; j<kiekPamoku; j++)
         {
-            if(pamokos[j].kiekisMokiniu>pamokos[max].kiekisMokiniu || (pamokos[j].kiekisMokiniu==pamokos[max].kiekisMokiniu && pamokos[j].pavadinimas>pamokos[max].pavadinimas))
+            if(pamokos[j].kiekVirs9>pamokos[max].kiekVirs9 || (pamokos[j].kiekVirs9==pamokos[max].kiekVirs9 && pamokos[j].pavadinimas<pamokos[max].pavadinimas))
             {
                 max=j;
             }
@@ -126,15 +145,27 @@ void Rikiavimas(int kiekPamoku, Pamoka*& pamokos)
 
 void Isvedimas(int kiekPamoku, Pamoka*& pamokos)
 {
+    int suma=0;
     for(int i=0; i<kiekPamoku; i++)
     {
-        cout << pamokos[i].pavadinimas << " " << pamokos[i].kiekisMokiniu << endl;
-        for(int j=0; j<pamokos[i].kiekisMokiniu; j++)
+        if(pamokos[i].kiekVirs9>0)
         {
-            if(pamokos[i].vidurkiai[j]>=9)
+            cout << pamokos[i].pavadinimas << " " << pamokos[i].kiekVirs9 << endl;
+            for(int j=0; j<pamokos[i].kiekisMokiniu; j++)
             {
-                cout << pamokos[j].mokiniai << endl;
+                if(pamokos[i].vidurkiai[j]>=9)
+                {
+                    cout << pamokos[i].mokiniai[j] << endl;
+                }
             }
         }
+    }
+    for(int j=0; j<kiekPamoku; j++)
+    {
+        suma+=pamokos[j].kiekVirs9;
+    }
+    if(suma==0)
+    {
+        cout << "Neatitika vidurkis " << endl;
     }
 }
